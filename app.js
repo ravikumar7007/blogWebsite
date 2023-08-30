7c1988bb72187aa103aeda2001785c5ab0b445ab
ravikumar7007 <ravikumarvelu2000@gmail.com>
2023-08-30 05:37:38 +0530

    Add Posts Array and render in home

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-
+const posts = [];
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent =
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("home", { homeSContent: homeStartingContent });
+  res.render("home", { homeSContent: homeStartingContent, postArray: posts });
 });
 app.get("/about", (req, res) => {
   res.render("about", { aboutContent: aboutContent });
@@ -35,8 +35,9 @@ app.post("/compose", (req, res) => {
     title: req.body.postTitle,
     content: req.body.postContent,
   };
-  console.log(obj);
-  res.render("compose");
+  posts.push(obj);
+
+  res.redirect("/");
 });
 app.listen(3000, function () {
   console.log("Server started on port 3000");
