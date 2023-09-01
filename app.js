e7c16b20e69640279d0bf49740b5abbdb117e451
ravikumar7007 <ravikumarvelu2000@gmail.com>
2023-09-01 22:01:16 +0530

    install mongoose

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const lodash = require("lodash");
+const mongoose = require("mongoose");
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/blogDB");
+}
+const post = mongoose.Schema({
+  title: String,
+  content: String,
+});
+const Post = mongoose.model("Post", post);
 const posts = [];
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -34,13 +45,17 @@ app.get("/contact", (req, res) => {
 app.get("/compose", (req, res) => {
   res.render("compose");
 });
+
 app.post("/compose", (req, res) => {
   const obj = {
     title: req.body.postTitle,
     content: req.body.postContent,
   };
-  posts.push(obj);
 
+  const post = new Post(obj);
+  post.save();
+
+  posts.push(obj);
   res.redirect("/");
 });
 
